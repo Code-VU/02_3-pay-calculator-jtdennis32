3504b62a0dca9a65830ee2501b2c8a2fc368184e
Jordan Dennis <Jordan.Dennis@vu.com>
2023-01-30 13:49:44 -0600

    calcPayJordan

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,10 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = int(input("Enter Hours:"))
+    rate = float(input("Enter Rate:"))
+    pay = rate * hrs
+    print(pay)
 
     # end assignment
 
